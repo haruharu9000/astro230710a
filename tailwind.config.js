77826722ef1e3a4c16cc86378dbb822f8fa210e5
haruharu9000 <nitori4baime@gmail.com>
2023-07-10 20:29:17 +0900

    aboutとblog

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+     content: ["src/**/*.{html,js,jsx,astro}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
